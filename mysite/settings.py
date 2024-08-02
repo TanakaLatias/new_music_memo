67af1736e70f5337f0e724f20acc1efb2d362121
Sawa <sawa@sawa.com>
2024-08-02 11:41:37 +0900

    fixed mysite/settings.py3 for render.com

--- mysite/settings.py
+++ mysite/settings.py
@@ -90,12 +90,17 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+"""
 if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     STATICFILES_DIRS = []
+"""
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = []
 print("----------------------------------------------------------------------------------------")
 print(DEBUG)
 print(STATICFILES_DIRS)
